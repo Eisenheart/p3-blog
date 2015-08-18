e95e1fb69726b9d8c50001eaf58b3c481fab274d
Derek Gerson <derekgrsn@gmail.com>
2015-08-17 17:42:10 -0700

    add working edit feature

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,15 +1,48 @@
 class ArticlesController < ApplicationController
   def index
+ 		@articles = Article.all
   end
 
+ 	def show
+ 		@article = Article.find(params[:id])
+ 	end
+
   def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def create
   	# render plain: params[:article].inspect
-  	@article = Article.new(params[:article])
+  	@article = Article.new(article_params)
 
-  	@article.save
-  	redirect_to @article
+  	if @article.save
+  	  redirect @article
+    else
+      render 'new'
+    end
   end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect @article
+    else
+      render 'edit'
+    end
+  end
+
+
+  private
+
+ 	def article_params
+ 		params.require(:article).permit(:title, :text)
+ 	end
+
+
+
+
 end
